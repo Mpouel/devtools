ee255a211a953f12477a883159f7ea6407ee1dbd
GeekCoder403 <124810233+Mpouel@users.noreply.github.com>
2025-04-01 10:10:32 -0400

    vb

--- devtools.js
+++ devtools.js
@@ -5,7 +5,7 @@ const oError = console.error;
 
 const devtools = $c("devtools")
 const iframe = $s("iframe");
-let tabs = $a(".pri .item")
+let tabs = $a(".pri .item");
 let console;
 
 function switchTab(nTab) {
